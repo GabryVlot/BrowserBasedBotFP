f93cbc97eae32058a6420b93b05121ef3006af6c
GabryVlot <vlot.gabry@gmail.com>
2018-05-28 22:36:30 +0200

    diffRefactoring

---
+++ tests/diffFP_test.js
@@ -0,0 +1,11 @@
+const diffFP = require('../analyse/diffFP.js');
+const testData = require('./test_data');
+
+const start = function(){
+    const sourceRow = testData.sourceRow;
+    const targetRow = testData.targetRow;
+
+    let result = diffFP.compareRecord('fp', sourceRow, targetRow);
+}
+
+start();
